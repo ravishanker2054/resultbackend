c3de78b99c543602266329c3750972ef7f3797c8
ravishanker2054 <52577397+ravishanker2054@users.noreply.github.com>
2022-05-22 23:03:09 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    var XLSX = require('xlsx');
+    var workbook = XLSX.readFile('Book3.xlsx');
+    var sheet_name_list = workbook.SheetNames;
+    var json_data=XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    });
+
+    res.end(JSON.stringify(json_data));
+    }
+);
+server.listen(8080);
+console.log('Server running at http://localhost:8080/');
